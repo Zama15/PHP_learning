522001974c9f90177e8a11efd437ff6d7012896b
Zama15 <alexavdstiles@gmail.com>
2024-02-23 13:06:27 -0600

    Add modal and no-structural logic
    It appear the modal if clicking the image of the vehicle.
    now support adding diferents types of specific data.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 const app = {
   urlDatos: 'data/autos.json',
-  filtros: 'todos',
+  filtro: 'todos',
+
+  modalFoto: document.querySelector('.modal'),
+  foto: document.getElementById('foto'),
 
   cargarFichas() {
     const fichas = document.getElementById('fichas');
@@ -9,19 +12,22 @@ const app = {
       .then(response => response.json())
       .then (autos => {
         autos.forEach(auto => {
-          if (this.filtros === 'todos' || auto.tipo === this.filtros) {
+          if (this.filtro === 'todos' || auto.tipo === this.filtro) {
+            keys = Object.keys(auto);
+            lastKey = keys[keys.length - 1];
+            data = auto[lastKey];
+            let datos ='';
+            for (let key in data) {
+              datos += `<span>${data[key]}</span>`;
+            }
             html += `
               <div class="ficha">
-                <img src="resources/imgs/${auto.foto}" alt="${auto.marca} ${auto.modelo}">
+                <img src="resources/imgs/${auto.foto}" alt="${auto.marca} ${auto.modelo}" onclick=app.verFoto('${auto.foto}')>
                 <div class="datos">
                   <h3>${auto.marca}</h3>
                   <span>${auto.modelo}</span>
                   <span>${auto.anio}</span>
-                  <p>
-                    ${auto.motor.desplazamiento}
-                    ${auto.motor.potencia}
-                    ${auto.motor.rendimiento}
-                  </p>
+                  <p>${datos}</p>       
                 </div>
               </div>
             `;
@@ -29,9 +35,34 @@ const app = {
         })
         fichas.innerHTML = html;
       }).catch(error => console.error('Error:', error));
+  },
+
+  verFoto(foto) {
+    this.foto.src = `resources/imgs/${foto}`;
+    this.modalFoto.style.display = 'block';
   }
 }
 
 window.onload = function() {
   app.cargarFichas();
+
+  const amenu = document.querySelectorAll("a.menu");
+
+  amenu.forEach(a => {
+    a.addEventListener('click', e => {
+      e.preventDefault();
+      app.filtro = a.getAttribute('data-filtro');
+      app.cargarFichas();
+    });
+  });
+
+  document.getElementById('close-modal').addEventListener('click', () => {
+    app.modalFoto.style.display = 'none';
+  });
+}
+
+window.onclick = event => {
+  if (event.target == app.modalFoto) {
+    app.modalFoto.style.display = 'none';
+  }
 }
