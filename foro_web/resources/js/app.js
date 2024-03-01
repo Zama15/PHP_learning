5cc1c030ac9243e2f3fe9720d604a4f1746b99d1
ALEJANDRO JERONIMO AZAMAR <ajeronimo@ucol.mx>
2024-03-01 11:04:44 -0600

    start foro project

---
+++ foro_web/resources/js/app.js
@@ -0,0 +1,10 @@
+const app = {
+  urlPost: 'http://jsonplaceholder.typicode.com/posts',
+  urlComments: 'http://jsonplaceholder.typicode.com/comments',
+  urlUsers: 'http://jsonplaceholder.typicode.com/users',
+
+  loadPosts: function() {
+    const cont = $('#content');
+    let html = '';
+  }
+}
---
+++ foro_web/resources/views/initialize.php
@@ -0,0 +1,21 @@
+<?php 
+  include "layouts/main_header.php";
+  include "layouts/main_footer.php";
+
+  head();
+?>
+    <div class="row mx-auto mt-5 z-1" style="width: 90%;">
+      <div class="col-8 shadow p-3 mb-5 bg-body-tartiary rounded">
+        <div class="content" id="content">
+          
+        </div>
+      </div>
+      <div class="col-4 shadow p-3 mb-5 bg-body-tartiary rounded">
+        <div class="authors list-groups" id="authors">
+          
+        </div>
+      </div>
+    </div>
+<?php 
+  foot();
+?>
